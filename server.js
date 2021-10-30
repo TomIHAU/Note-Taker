38075413754c8e9d658c9faf164aa089a039cddd
TauH <thaywood988@gmail.com>
2021-10-30 15:37:21 +1100

    started routing

--- server.js
+++ server.js
@@ -8,3 +8,21 @@ let database = require("./db/db.json");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.js"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+  getNotes;
+});
+app.post("/api/notes", (req, res) => {
+  saveNote;
+});
+app.delete("/api/notes", (req, res) => {
+  deleteNote;
+});
