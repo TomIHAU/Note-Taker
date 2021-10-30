66f50f77a0bcba46322fb1468177833825a0d092
TauH <thaywood988@gmail.com>
2021-10-30 16:26:05 +1100

    added function to get post and delete

--- server.js
+++ server.js
@@ -2,27 +2,36 @@ const path = require("path");
 const fs = require("fs");
 const express = require("express");
 const app = express();
-
-let database = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/index.js"))
-);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 app.get("/api/notes", (req, res) => {
-  getNotes;
+  res.sendFile(path.join(__dirname, "./db/db.json"));
 });
+
 app.post("/api/notes", (req, res) => {
-  saveNote;
+  let notePosted = req.body;
+  let notes = JSON.parse(fs.readFile("./db/db.json", "utf8"));
+  notePosted.id = uuidv4();
+  notes.push(notePosted);
+
+  fs.writeFile("./db/db.json", JSON.stringify(notes));
+  res.json(notes);
 });
-app.delete("/api/notes", (req, res) => {
-  deleteNote;
+
+app.delete("/api/notes:id", (req, res) => {
+  const notes = JSON.parse(fs.readFile("./db/db.json", "utf8"));
+  const keptNotes = notes.filter((note) => note.id != req.params.id);
+
+  fs.writeFile("./db/db.json", JSON.stringify(keptNotes));
+  res.json(keptNotes);
 });
